635200abf2b353b1695a9a7de1e87c25fe523a4b
João Vieira <itsjvieira@gmail.com>
2021-09-16 14:20:11 +0100

    Wallpaper, lock screen and login screen

--- config.sh
+++ config.sh
@@ -16,4 +16,9 @@ cp VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
 cp xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
 cp gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 cp gtkrc-2.0 ~/.gtkrc-2.0
+sudo cp etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 sudo cp etc/modprobe.d/blacklist.conf /etc/modprobe.d/blacklist.conf
+
+convert wallpapers/1366×768.jpg lock.png -gravity center -composite ~/.config/i3/lockscreen.png
+sudo cp wallpapers/1366×768.jpg /usr/share/backgrounds/wallpaper.jpg
+nitrogen --set-auto /usr/share/backgrounds/wallpaper.jpg --save
