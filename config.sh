414c9b5529a85a42cad98c9ade505568163559a6
João Vieira <itsjvieira@gmail.com>
2021-09-16 14:16:49 +0100

    GTK2 theme

--- config.sh
+++ config.sh
@@ -15,3 +15,4 @@ cp VSCodium/storage.json ~/.config/VSCodium/storage.json
 cp VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
 cp xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
 cp gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+cp gtkrc-2.0 ~/.gtkrc-2.0
