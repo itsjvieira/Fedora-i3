3396a5bdf9ddb5768b5b6d7587cf891c2c55d474
João Vieira <itsjvieira@gmail.com>
2021-09-16 16:26:43 +0100

    Move configuration files to the directory config

--- config.sh
+++ config.sh
@@ -9,12 +9,12 @@ mkdir -p ~/.config/VSCodium/User
 mkdir -p ~/.config/xfce4/terminal
 mkdir -p ~/.config/gtk-3.0
 
-cp i3/config ~/.config/i3/config
-cp i3status/config ~/.config/i3status/config
-cp VSCodium/storage.json ~/.config/VSCodium/storage.json
-cp VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
-cp xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
-cp gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+cp config/i3/config ~/.config/i3/config
+cp config/i3status/config ~/.config/i3status/config
+cp config/VSCodium/storage.json ~/.config/VSCodium/storage.json
+cp config/VSCodium/User/settings.json ~/.config/VSCodium/User/settings.json
+cp config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
+cp config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 cp gtkrc-2.0 ~/.gtkrc-2.0
 sudo cp etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 sudo cp etc/modprobe.d/blacklist.conf /etc/modprobe.d/blacklist.conf
